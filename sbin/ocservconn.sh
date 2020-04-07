0442ae92c9ec1108c1c8dbd8f94074e4da891598
ChengWF <chengwf@lnmos.com>
2020-04-07 08:23:47 +0000

    修正VPN客户端连接时，服务端脚本关系应用

--- sbin/ocservconn.sh
+++ sbin/ocservconn.sh
@@ -20,13 +20,13 @@ fi
 
 if [ "$REASON" = "connect" ];then
    # Disable the firewall for the user's device
-   $ipset add vpnpolicy_src_$getid $IP_REMOTE >/dev/null 2>&1
+   $ipset add VPN_Client_$getid $IP_REMOTE >/dev/null 2>&1
    # writelog
    $pytools $wkdir/tools/API.py API wrtvpnlogin "Ocserv" "登录成功,分配地址:$IP_REMOTE" $USERNAME $IP_REAL >/dev/null 2>&1
    # Reload User StaticRoute
    $wkdir/sbin/AdvRouteAPI.sh onlySR >/dev/null 2>&1
 else
-   $ipset del vpnpolicy_src_$getid $IP_REMOTE >/dev/null 2>&1
+   $ipset del VPN_Client_$getid $IP_REMOTE >/dev/null 2>&1
    # writelog
    $pytools $wkdir/tools/API.py API wrtvpnlogin "Ocserv" "退出登录,释放地址:$IP_REMOTE" $USERNAME $IP_REAL >/dev/null 2>&1
 fi
